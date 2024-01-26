188c818907f7a66dbd9a5469f2a6ac159d25a1f1
ahmed-farrag <ahmedfaarrag@gmail.com>
2024-01-26 20:56:53 +0200

    movie list

--- src/__test__/Utils/mswMockReq.js
+++ src/__test__/Utils/mswMockReq.js
@@ -1,11 +1,12 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-const makeReq = (config) => {
+const makeReq = async (config) => {
   const handler = config.map((con) => {
     return rest[con.method || "get"](con.url, (req, res, ctx) => {
       console.log("this query" + req.url.searchParams.get("query"));
-      return res(ctx.json(con.data));
+      // return res(ctx.json(con.data));
+      return res(ctx.json(con.response));
     });
   });
   const server = setupServer(...handler);
